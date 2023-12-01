2f7af3ad929dc7afbc32d50c5cd3ae7014affb63
Euny Kim <euny4535@gmail.com>
2023-12-01 12:02:13 +0900

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.hotelreservationprogramme"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hotelreservationprogramme"
         minSdk = 26
-        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 

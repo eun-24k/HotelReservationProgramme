add664185c252ea1f45058f27dddf6b2687e2a18
Euny Kim <euny4535@gmail.com>
2023-12-03 22:31:41 +0900

    lv3 in progress
    - lv1 부분에서 못다한 랜덤한 입출금 내역을 추가해 주었다
    - lv3 부분도 완성하였다.
    - lv2 부분에 미흡한 부분이 있어서 추가 수정 필요

--- app/src/main/java/com/example/hotelreservationprogramme/HotelReservationProgrammeLv3.kt
+++ app/src/main/java/com/example/hotelreservationprogramme/HotelReservationProgrammeLv3.kt
@@ -15,6 +15,7 @@ open class MainMenu {
 
         while (true) {
             println("호텔예약 프로그램 입니다.")
+            println("[메뉴]")
             println("1. 방예약, 2. 예약목록 출력, 3 예약목록 (정렬) 출력, 4. 시스템 종료, 5. 금액 입금-출금 내역 목록 출력 6. 예약 변경/취소")
 
             val readLineExceptionInt = ReadLineExceptionInt()
@@ -74,19 +75,21 @@ class Menu1 : MainMenu() {
         val checkInDateObject = CheckInDate()
         checkInDate = checkInDateObject.checkNumber(checkInDate)
 
-
         println("체크아웃 날짜를 입력해주세요 표기형식. 20230631")
         var checkOutDate = readLinExceptionInt.checkIntException()
         val checkOutDateObject = CheckOutDate()
         checkOutDate = checkOutDateObject.checkNumber(checkInDate, checkOutDate)
 
-        val user = BookingInfo(name, roomNumber, checkInDate, checkOutDate)
+        val paymentIn: Int = (10000..1000000).random()
+        val paymentOut: Int = (10000..1000000).random()
+
+        val user = BookingInfo(name, roomNumber, checkInDate, checkOutDate, paymentIn, paymentOut)
 
         return user
     }
 }
 
-class Menu2 {
+class Menu2 : MainMenu() {
     fun menu2(bookingInfo: MutableList<BookingInfo>) {
         println("호텔 예약자 목록입니다.")
         for (i in 0 until bookingInfo.size) {
@@ -108,16 +111,29 @@ class Menu3 {
 
 class Menu5 {
     fun menu5(bookingInfo: MutableList<BookingInfo>) {
-        println("조회하실 사용자 이름을 입력하세요.")
-        val checkName = readLine()!!
-    }
+        val checkString = CheckName()
+        val userIndex = checkString.checkString(bookingInfo)
+        val (_, _, _, _, paymentIn, paymentOut) = bookingInfo[userIndex]
 
+        if (userIndex > 0) {
+            println("1. 초기 금액으로 $paymentIn 원 입금되었습니다.")
+            println("2. 예약금으로 $paymentOut 원 출금되었습니다.")
+        } else {
+            println("조회하시는 사용자를 찾을 수 없습니다.")
+        }
+    }
 }
+
 
 // 데이터 클래스 -------------------------------------------------------------------------------------
 
 data class BookingInfo(
-    var name: String, var roomNumber: Int, var checkInDate: Int, var checkOutDate: Int
+    var name: String,
+    var roomNumber: Int,
+    var checkInDate: Int,
+    var checkOutDate: Int,
+    var paymentIn: Int,
+    var paymentOut: Int
 )
 
 // 인터페이스 ---------------------------------------------------------------------------------------
@@ -132,6 +148,10 @@ interface CheckNumberOneInput {
 
 interface CheckNumberTwoInput {
     fun checkNumber(value1: Int, value2: Int): Int
+}
+
+interface CheckBookingInfoInput {
+    fun checkString(bookingInfo: MutableList<BookingInfo>): Int
 }
 
 // 클래스 -------------------------------------------------------------------------------------------
@@ -154,7 +174,7 @@ class RoomNumber : CheckNumberOneInput {
 class CheckInDate : CheckNumberOneInput {
     override fun checkNumber(value1: Int): Int {
         var checkInDate = value1
-        var todayDate: Int = 20231201
+        val todayDate: Int = 20231201
         val readLineExceptionInt = ReadLineExceptionInt()
 
         while (true) {
@@ -184,6 +204,24 @@ class CheckOutDate : CheckNumberTwoInput {
         }
     }
 }
+
+class CheckName : CheckBookingInfoInput {
+    override fun checkString(bookingInfo: MutableList<BookingInfo>):Int {
+        println("조회하실 사용자 이름을 입력하세요.")
+        var checkName = readLine()!!
+        var userIndex = -1
+        for (i in bookingInfo.indices) {
+            val (name) = bookingInfo[i]
+            if (checkName == name) {
+                userIndex = i
+                break
+            }
+        }
+        return userIndex
+    }
+
+}
+
 
 class ReadLineExceptionInt : CheckIntException {
     override fun checkIntException(): Int {
